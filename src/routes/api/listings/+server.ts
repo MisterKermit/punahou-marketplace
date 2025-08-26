2dba7c9002f94a79a763a5d191185b8ca69ed08a
Ruuku420 <pookie@click4porn.org>
2025-08-25 19:30:44 -1000

    Bug fixes in POST and added DELETE, PUT, fallback

--- src/routes/api/listings/+server.ts
+++ src/routes/api/listings/+server.ts
@@ -4,16 +4,26 @@ import type { RequestHandler } from './$types';
 import { v4 as uuidv4, validate } from 'uuid';
 import { db } from "../../../services/database.ts";
 
+type ListingID = string | null;
+
+const validate_listingId = (listingId: listingId, callback) => {
+  if (validate(listingId)) {
+    return callback();
+  } else {
+    return error(400, { message: "Query id is not valid" });
+  }
+}
+
+
+
 export const GET: RequestHandler = async ({ url }) => {
-  const listingId: string | null = url.searchParams.get('id'); // /listings?id={id}
+  const listingId: string | null = url.searchParams.get('id');
 
   if (listingId) {
-    if (validate(listingId)) {
+    return validate_listingId(listingId, async () => {
       const res = await db.get(["listings", listingId]);
-      return new json(res);
-    } else {
-      return new error(400, { message: "Query id is not valid" });
-    }
+      return json(res);
+    });
   }
 
   const res = await Array.fromAsync(
@@ -23,7 +33,45 @@ export const GET: RequestHandler = async ({ url }) => {
   return json(res);
 };
 
-export const POST: RequestHandler = async ({ req }) => {
+export const POST: RequestHandler = async ({ request }) => {
   const listingToAdd = await request.json();
-  const res = await db.set(["listings", uuidv4()], listingToAdd);
+  const id_ = uuidv4();
+
+  const res = await db.set(["listings", id_], listingToAdd);
+  return json(res);
+};
+
+// Probably no need for PUT
+export const PATCH: RequestHandler = async ({ request, url }) => {
+  const changes = await request.json(); // TODO: Validate changes
+
+  const listingId: string | null = url.searchParams.get('id');
+
+  if (listingId) {
+    return validate_listingId(listingId, async () => {
+      const currentData = await db.get(["listings", listingId]);
+
+      const res = await db.set(
+        ["listings", listingId],
+        Object.assign(currentData.value, changes)
+      );
+
+      return json(res);
+    });
+  }
+}
+
+export const DELETE: RequestHandler = async ({ request, url }) => {
+  const listingId: string | null = url.searchParams.get('id');
+
+  if (listingId) {
+    return validate_listingId(listingId, async () => {
+      await db.delete(["listings", listingId]);
+      return json({ "message": "success" })
+    });
+  }
+}
+
+export const fallback: RequestHandler = async ({ request }) => {
+	return error(415, `${request.method} unsupported!`);
 };
